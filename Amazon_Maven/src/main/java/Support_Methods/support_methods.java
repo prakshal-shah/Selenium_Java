103a9aa547db49d9df502b7b7326d4e3cf50a17e
prakshal-shah <prakshal.einfochips@gmail.com>
2021-07-26 09:48:52 +0530

    Added function to find element

--- Amazon_Maven/src/main/java/Support_Methods/support_methods.java
+++ Amazon_Maven/src/main/java/Support_Methods/support_methods.java
@@ -5,86 +5,60 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class support_methods {
 
-	public WebDriver driver;
-
+	private WebDriver driver;
+	private WebElement webElement;
 	public void clickElement(String element, String findBy, WebDriver driver) {
+
+		webElement = find_element(element, findBy);
+		webElement.click();
+		
+
+	}
+
+	public void sendKeys(String element, String write, String findBy, WebDriver driver) {
+		
+		webElement = find_element(element, findBy);
+		webElement.sendKeys(write);
+
+	}
+
+	public void pressEnter(String element, String findBy, WebDriver driver) {
+		webElement = find_element(element, findBy);
+		webElement.sendKeys(Keys.ENTER);
+		
+	}
+	
+	public WebElement find_element(String element, String findBy) {
 		switch (findBy) {
 		case "xpath":
-			driver.findElement(By.xpath(element)).click();
+			 webElement = driver.findElement(By.xpath(element));
 			break;
 		case "id":
-			driver.findElement(By.id(element)).click();
+			webElement = driver.findElement(By.id(element));
 			break;
 		case "css":
-			driver.findElement(By.cssSelector(element)).click();
+			webElement = driver.findElement(By.cssSelector(element));
 			break;
 		case "name":
-			driver.findElement(By.name(element)).click();
+			webElement = driver.findElement(By.name(element));
 			break;
 		case "class_name":
-			driver.findElement(By.className(element)).click();
+			webElement = driver.findElement(By.className(element));
 			break;
 		case "linkText":
-			driver.findElement(By.linkText(element)).click();
+			webElement = driver.findElement(By.linkText(element));
 		}
-
+		return webElement;
 	}
-
-	public void sendKeys(String element, String write, String findby, WebDriver driver) {
-		switch (findby) {
-
-		case "xpath":
-			driver.findElement(By.xpath(element)).sendKeys(write);
-			break;
-		case "id":
-			driver.findElement(By.id(element)).sendKeys(write);
-			break;
-		case "css":
-			driver.findElement(By.cssSelector(element)).sendKeys(write);
-			break;
-		case "name":
-			driver.findElement(By.name(element)).sendKeys(write);
-			break;
-		case "class_name":
-			driver.findElement(By.className(element)).sendKeys(write);
-			break;
-		case "linkText":
-			driver.findElement(By.linkText(element)).sendKeys(write);
-			break;
-		}
-
-	}
-
-	public void pressEnter(String element, String findby, WebDriver driver) {
-		switch (findby) {
-
-		case "xpath":
-			driver.findElement(By.xpath(element)).sendKeys(Keys.ENTER);
-			break;
-		case "id":
-			driver.findElement(By.id(element)).sendKeys(Keys.ENTER);
-			break;
-		case "css":
-			driver.findElement(By.cssSelector(element)).sendKeys(Keys.ENTER);
-			break;
-		case "name":
-			driver.findElement(By.name(element)).sendKeys(Keys.ENTER);
-			break;
-		case "class_name":
-			driver.findElement(By.className(element)).sendKeys(Keys.ENTER);
-			break;
-		case "linkText":
-			driver.findElement(By.linkText(element)).sendKeys(Keys.ENTER);
-		}
-	}
+	
 	public void close_popup() {
 		try {
 			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 //			log.debug("Closing popups");
-//			driver.findElement(By.xpath("//*[@data-action-type='DISMISS']")).click();
 			clickElement("//*[@data-action-type='DISMISS']", "xpath", driver);
 //			log.info("popup closed");
 		} catch (Exception e) {
